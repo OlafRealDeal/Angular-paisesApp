60a355b99632ff9ce71e5549f4c585bc3b09a583
Alvaro <almaflomon@gmail.com>
2022-10-03 17:21:01 -0400

    Seccion 9 terminada

---
+++ src/app/pais/interfaces/pais.interface.ts
@@ -0,0 +1,168 @@
+// export interface Country {
+//     name:         Name;
+//     tld:          string[];
+//     cca2:         string;
+//     ccn3:         string;
+//     cca3:         string;
+//     cioc:         string;
+//     independent:  boolean;
+//     status:       string;
+//     unMember:     boolean;
+//     currencies:   Currencies;
+//     idd:          Idd;
+//     capital:      string[];
+//     altSpellings: string[];
+//     region:       string;
+//     subregion:    string;
+//     languages:    Languages;
+//     translations: { [key: string]: Translation };
+//     latlng:       number[];
+//     landlocked:   boolean;
+//     borders:      string[];
+//     area:         number;
+//     demonyms:     Demonyms;
+//     flag:         string;
+//     maps:         Maps;
+//     population:   number;
+//     gini:         Gini;
+//     fifa:         string;
+//     car:          Car;
+//     timezones:    string[];
+//     continents:   string[];
+//     flags:        CoatOfArms;
+//     coatOfArms:   CoatOfArms;
+//     startOfWeek:  string;
+//     capitalInfo:  CapitalInfo;
+// }
+
+// export interface CapitalInfo {
+//     latlng: number[];
+// }
+
+// export interface Car {
+//     signs: string[];
+//     side:  string;
+// }
+
+// export interface CoatOfArms {
+//     png: string;
+//     svg: string;
+// }
+
+// export interface Currencies {
+//     BOB: Bob;
+// }
+
+// export interface Bob {
+//     name:   string;
+//     symbol: string;
+// }
+
+// export interface Demonyms {
+//     eng: Eng;
+//     fra: Eng;
+// }
+
+// export interface Eng {
+//     f: string;
+//     m: string;
+// }
+
+// export interface Gini {
+//     "2019": number;
+// }
+
+// export interface Idd {
+//     root:     string;
+//     suffixes: string[];
+// }
+
+// export interface Languages {
+//     aym: string;
+//     grn: string;
+//     que: string;
+//     spa: string;
+// }
+
+// export interface Maps {
+//     googleMaps:     string;
+//     openStreetMaps: string;
+// }
+
+// export interface Name {
+//     common:     string;
+//     official:   string;
+//     nativeName: NativeName;
+// }
+
+// export interface NativeName {
+//     aym: Translation;
+//     grn: Translation;
+//     que: Translation;
+//     spa: Translation;
+// }
+
+// export interface Translation {
+//     official: string;
+//     common:   string;
+// }
+
+export interface Country {
+    name:           string;
+    topLevelDomain: string[];
+    alpha2Code:     string;
+    alpha3Code:     string;
+    callingCodes:   string[];
+    capital:        string;
+    altSpellings:   string[];
+    region:         string;
+    subregion:      string;
+    population:     number;
+    latlng:         number[];
+    demonym:        string;
+    area:           number;
+    gini:           number;
+    timezones:      string[];
+    borders:        string[];
+    nativeName:     string;
+    numericCode:    string;
+    currencies:     Currency[];
+    languages:      Language[];
+    translations:   Translations;
+    flag:           string;
+    regionalBlocs:  RegionalBloc[];
+    cioc:           string;
+}
+
+export interface Currency {
+    code:   string;
+    name:   string;
+    symbol: string;
+}
+
+export interface Language {
+    iso639_1:   string;
+    iso639_2:   string;
+    name:       string;
+    nativeName: string;
+}
+
+export interface RegionalBloc {
+    acronym:       string;
+    name:          string;
+    otherAcronyms: any[];
+    otherNames:    any[];
+}
+
+export interface Translations {
+    de: string;
+    es: string;
+    fr: string;
+    ja: string;
+    it: string;
+    br: string;
+    pt: string;
+    nl: string;
+    hr: string;
+    fa: string;
+}
